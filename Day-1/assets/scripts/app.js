55300f37cd130e2137947e7a56c4a172465658e3
Stewart Michaels <steward.kama@gmail.com>
2022-10-26 14:00:04 +0530

    day-2 commit

--- Day-1/assets/scripts/app.js
+++ Day-1/assets/scripts/app.js
@@ -1,5 +1,11 @@
 let currentResult = 0;
 
+function add(num1, num2) {
+    const result = num1 + num2;
+    return result;
+}
+
+
 currentResult = currentResult + 7;
 
 let calculationDescription = '0 + 7';
---
+++ Day-2/basics-assignment-1-start/assignment.js
@@ -0,0 +1,11 @@
+let userInput = 10;
+let result = userInput + 18;
+
+result = result - 10;
+
+result = result * 10;
+
+result = result / 10;
+
+alert(result);
+alert(userInput); 
